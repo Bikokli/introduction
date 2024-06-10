4ed24b81be98bdced9c232a355306f6e7690915d
bikokli <Bikokli9@bmail.com>
2024-06-11 01:30:42 +0300

    INTRODUCT

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        //первое задание
+        // сылка на задачу
+        // https://github.com/netology-code/java-homeworks/tree/video/introduction/1.1.1
+        System.out.println("Первое задание");
+        System.out.println("Петров Николай");
+        System.out.println();
+
+        //второе задание
+        //сылка на задачу
+        //https://github.com/netology-code/java-homeworks/tree/video/introduction/1.1.2
+
+        System.out.println("Второе задание");
+        int value1=152;
+        int value2=137;
+        System.out.println(value1-value2);
+    }
+}
